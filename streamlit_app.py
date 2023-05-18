2ab2560be9efb4a72e91a7bd52831358eb6a2993
reyahman010 <133841822+reyahman010@users.noreply.github.com>
2023-05-18 14:40:14 +0900

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #  This will not work correctly, but just go with it for now
-my_cur.excute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
